7ba6f5d5b74e2933c7f14527619c0fce83b5f085
isngo <isngo@s.sfusd.edu>
2025-04-24 13:46:54 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,19 @@
 public class APLine
 {
   /* your code here */
+  private int myA;
+  private int myB;
+  private int myC;
+  public APLine(int a, int b, int c){
+    myA = a;
+    myB = b;
+    myC = c;
+  }
+  public double getSlope(){
+    return (-(double)myA)/myB;
+  }
+  public boolean isOnLine(int x, int y){
+    return myA*x + myB*y + c == 0;
+  }
   
 }
